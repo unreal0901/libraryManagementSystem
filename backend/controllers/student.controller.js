70f857d9142355cfd966f7fdc06e3a0f552e8c74
aditya <rawatas0901@gmail.com>
2023-05-14 17:45:42 +0530

    Will return No book available message when there are no more books to issue , in future we will add reservation for books for that user

--- backend/controllers/student.controller.js
+++ backend/controllers/student.controller.js
@@ -7,6 +7,11 @@ module.exports.issueHandler = async (req, res, next) => {
     const bookId = req.params.bookId;
     const user = res.locals.user;
     const book = await findBook({ _id: bookId });
+
+    if (book.numBooksAvailable === 0) {
+      return res.status(404).json({ message: "No available books to issue." });
+    }
+
     await issueBook(user, book);
     res.status(201).json({
       message: `Book ${book.title} issued by ${user.fullName}`,
